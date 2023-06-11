eb5fe7de42876f3b53eae37659c004c7f0aa2c71
farhadis <goshayesh@gmail.com>
2023-06-11 12:09:51 +0200

    ajout des icons move et select

--- FrontEnd/assets/script.js
+++ FrontEnd/assets/script.js
@@ -161,9 +161,12 @@ logout();
 
 var modal = document.getElementById("myModal");
 var modal2 = document.getElementById("myModal2");
+// var btnEdite = document.getElementById("myBtn");
 var btnClose = document.getElementsByClassName("close")[0];
 var btnClose2 = document.getElementsByClassName("close2")[0];
 
+// const titre = document.querySelector("#titre");
+// const categorie = document.querySelector("#categorie");
 
 
 
@@ -207,19 +210,11 @@ async function modal1(){
       figure.appendChild(image);
       figure.appendChild(figcaption);
 
+     
+	  if (i = i) {
+            move.style.visibility = "collapse";
+	    } 
     
-
-      figure.addEventListener("mouseover", function(){
-        move.style.visibility = "visible";
-      })
-      
-      figure.addEventListener("mouseout", function(){
-        move.style.visibility = "collapse"
-      })
-
-
-      
-
       trash.addEventListener("click", (event) => {
         event.preventDefault();
             
@@ -232,14 +227,6 @@ async function modal1(){
   
   };
   
-}
-// location.reload()
-function moveAct(){
-    const figure = document.createElement("figure");
-    figure.appendChild(move);
-    if(figure.file[i] == 0){
-        move.style.visibility = "visible";  
-    }
 }
 
 
@@ -381,7 +368,7 @@ function projetForm() {
         e.preventDefault();
         
             if ((titre.value !== "") && (categorie.value !== "0") && (inputImage.file !== "")) {
-    
+            
              
             const formData = objetFormData();
             const reponse = await addProjet(formData);
@@ -393,7 +380,9 @@ function projetForm() {
         } else{ 
             document.querySelector("#error-txt-valider").innerHTML = "Veuillez  remplir bien le formulaire" ;
             supprimerMessage()
-              
+            
+           
+                 
         }   
        
     });  
@@ -437,6 +426,8 @@ function imageInput() {
 imageInput();
 
 
+
+
 // 
 function supprimerMessage(){
     const image = document.getElementById("file")
@@ -455,8 +446,6 @@ function supprimerMessage(){
 };
 
 
-
-// Function pour creer une prévisualisation
 function preview(e) {
 
     const input = document.querySelector(".img-modal2");
@@ -526,8 +515,84 @@ async function supprimerGalerie(){
 }
 
 
+function validateForm() {
+    let x = document.forms["myForm"]["fname"].value;
+    if (x == "") {
+      alert("Name must be filled out");
+      return false;
+    }
+  }
 
 
 
+// .....   SELECT   ..................................................
 
+var x, j, l, ll, selElmnt, a, b, c;
+x = document.getElementsByClassName("custom-select");
+l = x.length;
+for (i = 0; i < l; i++) {
+  selElmnt = x[i].getElementsByTagName("select")[0];
+  ll = selElmnt.length;
+  a = document.createElement("DIV");
+  a.setAttribute("class", "select-selected");
+  a.innerHTML = selElmnt.options[selElmnt.selectedIndex].innerHTML;
+  x[i].appendChild(a);
+  b = document.createElement("DIV");
+  b.setAttribute("class", "select-items select-hide");
+  for (j = 1; j < ll; j++) {
 
+    c = document.createElement("DIV");
+    c.innerHTML = selElmnt.options[j].innerHTML;
+    c.addEventListener("click", function(e) {
+
+        var y, i, k, s, h, sl, yl;
+        s = this.parentNode.parentNode.getElementsByTagName("select")[0];
+        sl = s.length;
+        h = this.parentNode.previousSibling;
+        for (i = 0; i < sl; i++) {
+          if (s.options[i].innerHTML == this.innerHTML) {
+            s.selectedIndex = i;
+            h.innerHTML = this.innerHTML;
+            y = this.parentNode.getElementsByClassName("same-as-selected");
+            yl = y.length;
+            for (k = 0; k < yl; k++) {
+              y[k].removeAttribute("class");
+            }
+            this.setAttribute("class", "same-as-selected");
+            break;
+          }
+        }
+        h.click();
+    });
+    b.appendChild(c);
+  }
+  x[i].appendChild(b);
+  a.addEventListener("click", function(e) {
+      e.stopPropagation();
+      closeAllSelect(this);
+      this.nextSibling.classList.toggle("select-hide");
+      this.classList.toggle("select-arrow-active");
+    });
+}
+function closeAllSelect(elmnt) {
+
+  var x, y, i, xl, yl, arrNo = [];
+  x = document.getElementsByClassName("select-items");
+  y = document.getElementsByClassName("select-selected");
+  xl = x.length;
+  yl = y.length;
+  for (i = 0; i < yl; i++) {
+    if (elmnt == y[i]) {
+      arrNo.push(i)
+    } else {
+      y[i].classList.remove("select-arrow-active");
+    }
+  }
+  for (i = 0; i < xl; i++) {
+    if (arrNo.indexOf(i)) {
+      x[i].classList.add("select-hide");
+    }
+  }
+}
+
+document.addEventListener("click", closeAllSelect);
